5baa582b2087db65ee7ad339c557a7b813b0f414
menidas98 <menidas98@gmail.com>
2019-01-31 15:46:45 +0100

    Moved controls content to a resource file

---
+++ SemiTransparentImgDisplay/Helpers/Converters/WindowStateToHeaderConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SemiTransparentImgDisplay.Helpers.Converters
+{
+    public class WindowStateToHeaderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                WindowState state = (WindowState)value;
+
+                return state == WindowState.Maximized ? "Minimize" : "Maximize";
+            }
+
+            return "Error in " + nameof(WindowStateToHeaderConverter.Convert);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
